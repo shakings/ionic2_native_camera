f9ece11c18fada60ba96b5ce77acf35a64233ed7
Stephen Engelbrecht <shakings@hotmail.co.za>
2017-07-03 14:52:16 +0200

    commit files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
+ 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    templateUrl: './home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+ 
+    public base64Image: string;
+ 
+    constructor(public navCtrl: NavController, private camera: Camera) {
+        this.base64Image = "https://placehold.it/150x150";
+    } 
+   
+    public takePicture() {
+     this.camera.getPicture({
+        quality : 75,
+        destinationType : this.camera.DestinationType.DATA_URL,
+        sourceType : this.camera.PictureSourceType.CAMERA,
+        allowEdit : true,
+        encodingType: this.camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        saveToPhotoAlbum: false
+        }).then(imageData => {
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+        }, error => {
+        console.log("ERROR -> " + JSON.stringify(error));
+        });
+    }
 }
